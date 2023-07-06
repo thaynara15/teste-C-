c1f0c0dea229de6a4fae7129776537816f3c0a0b
Thaynara <104026459+thaynara15@users.noreply.github.com>
2023-07-06 15:47:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace variaveis
+{
+    class Program
+    {
+        private static string nome;
+        private static bool cadastrado;
+
+        static void Main(string[] args)
+        {
+            string nome = "Thaynara Martins de Brito";
+            int idade = 17;
+            char sexo = 'M';
+            double salario = 2.500;
+            bool cadastrado = true; //true \\ false
+
+            Console.WriteLine(nome);
+
+            nome = "Thaynara Martins de Brito";
+            Console.WriteLine(nome);
+
+            //Console.WriteLine("Meu nome é: " + nome);
+            //+ = Concatenação 
+
+            //Console.WriteLine("Meu nome é: " + nome + " e tenho : " + idade + " anos");
+
+            //Console.WriteLine(salario);
+
+            Console.ReadKey();
+        }
+    }
+}
